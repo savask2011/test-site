07feb3edde90f2507205ad873eb7dcd5fda1e01c
Savas <savas@webites.co.uk>
2018-09-06 09:15:25 +0100

    Up to babel implementation

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,9 @@
+var $ = require('jquery');
+var Person = require('./modules/Person');
+
+var john = new Person("John Doe", "blue");
+john.greet();
+
+var jane = new Person("Jane Smith", "green");
+jane.greet();
+
