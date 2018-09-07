dfb4b4c3cdbea58827bf1fecd66a5144cecef5f3
Savas <savas@webites.co.uk>
2018-09-07 17:52:49 +0100

    Upto mobile menu icon.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,19 +1,4 @@
-var $ = require('jquery');
-import Person from './modules/Person';
+import $ from 'jquery';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes $0 in taxes.");
-  }
-}
-
-alert("ABC 321");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
